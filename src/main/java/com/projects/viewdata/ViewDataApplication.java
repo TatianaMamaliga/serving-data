0b3e4287fdda0e09cdcfa3e2c32d0764a2ae8dc5
Tatiana Mamaliga <mamaliga.tatiana@gmail.com>
2024-02-17 13:25:05 +0200

    initial commit

---
+++ src/main/java/com/projects/viewdata/ViewDataApplication.java
@@ -0,0 +1,13 @@
+package com.projects.viewdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewDataApplication.class, args);
+	}
+
+}
